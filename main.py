47796a4ec981ab700387f95b36f873dc1653939e
KoYu1 <konoeyume@gmail.com>
2026-08-09 17:06:32 +1000

    task 4

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
         log_state()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -21,6 +24,8 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
